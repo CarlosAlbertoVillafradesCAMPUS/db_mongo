1c770a57abb8d27d67f803de0b59ed39abbb1cd1
CarlosVillafradesCAMPUS <cavillafrades@gmail.com>
2023-08-08 13:30:11 -0500

    feat(MongoDB): :sparkles: Modificacion y Creacion de consultas
    se termino de modificar la base de datos con los shemas y empezamos  realizar los endopints para las consultas
    
    by:Carlos Villafrades

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
-// import dotenv from "dotenv";
-// import express from "express";
-// import storageAutomovil from "./routers/automovil.js";
-// 
-
-// dotenv.config();
-// const appExpress = express();
-
-// appExpress.use("/automovil", storageAutomovil)
-
-// let config = JSON.parse(process.env.MY_CONFIG);
-// appExpress.listen(config, ()=>console.log(`http://${config.hostname}:${config.port}`))
+import dotenv from "dotenv";
+import express from "express";
+import storageAutomovil from "./routers/automovil.js";
 
 
-import {con} from "./db/connect.js";
-let db = await con()
-let res = await db.createCollection("prueba");
-console.log(res);
+dotenv.config();
+const appExpress = express();
+
+appExpress.use(express.json());
+appExpress.use("/automovil", storageAutomovil)
+appExpress.use("/cliente", storageAutomovil)
+
+let config = JSON.parse(process.env.MY_CONFIG);
+appExpress.listen(config, ()=>console.log(`http://${config.hostname}:${config.port}`))
+
--- routers/automovil.js
+++ routers/automovil.js
@@ -1,14 +1,66 @@
 import dotenv from "dotenv";
 import {Router} from "express";
 import { configGet } from "../middleware/limit.js";
+import {con} from "../db/connect.js"
+
+let db = await con();
 
 dotenv.config();
 let storageAutomovil = Router();
 
-storageAutomovil.get("/", configGet(), async (req,res)=>{
-    if (!req.rateLimit) return;
-    console.log(req.rateLimit);
-    res.send("funcionaaaa")
+storageAutomovil.get("/", async (req,res)=>{
+    try {
+        const collection = db.collection("reserva");
+        let data = await collection.aggregate([
+            {
+              $match: {
+                Estado: "Pendiente",
+              },
+            },
+            {
+              $lookup: {
+                from: "cliente",
+                localField: "ID_Cliente_id",
+                foreignField: "ID_Cliente",
+                as: "Cliente_Info",
+              },
+            },
+            {
+              $unwind: "$Cliente_Info",
+            },
+            {
+              $lookup: {
+                from: "automovil",
+                localField: "ID_Automovil_id",
+                foreignField: "ID_Automovil",
+                as: "Automovil_Info",
+              },
+            },
+            {
+              $unwind: "$Automovil_Info",
+            },
+            {
+              $project: {
+                  "ID_Cliente_id":0,
+                  "ID_Automovil_id":0,
+                "Cliente_Info.ID_Cliente": 0,
+                "Automovil_Info.ID_Automovil": 0,
+              },
+            },
+          ]).toArray();
+        res.send(data);
+    } catch (error) {
+        res.send(error)
+        // if (error.errInfo.details.schemaRulesNotSatisfied[0].propertiesNotSatisfied) {
+        //     res.status(401).send(error.errInfo.details.schemaRulesNotSatisfied[0].propertiesNotSatisfied[0].description)
+        // }else{
+        //     res.status(401).send("es obligatorio el campo: " + error.errInfo.details.schemaRulesNotSatisfied[0].missingProperties)
+        // }
+       
+    }
+   
 })
+
+
 
 export default storageAutomovil;
